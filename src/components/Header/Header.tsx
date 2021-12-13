e3286992f5b70087d6a2c4765678e0e8bf9fd26f
DBarry-Code <dabrowski.barry@gmail.com>
2021-12-13 16:37:28 +0100

    react/icons include and prototype navbar done

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,80 @@
 import React from "react";
+import { FaBars } from "react-icons/fa";
 
 function Header() {
+    const [navbarOpen, setNavbarOpen] = React.useState(false);
+
     return (
         <header>
-            <h1>Navbar</h1>
+            <nav className='relative flex flex-wrap items-center justify-between px-2 py-3 bg-zinc-800 shadow-sm mb-3'>
+                <div className='container px-4 mx-auto flex flex-wrap items-center justify-between'>
+                    <div className='w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start'>
+                        <a
+                            className='text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white'
+                            href='home'
+                        >
+                            Barry Dabrowski
+                        </a>
+                        <button
+                            className='text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none'
+                            type='button'
+                            onClick={() => setNavbarOpen(!navbarOpen)}
+                        >
+                            <FaBars />
+                        </button>
+                    </div>
+                    <div
+                        className={
+                            "lg:flex flex-grow items-center" +
+                            (navbarOpen ? " flex" : " hidden")
+                        }
+                        id='example-navbar-danger'
+                    >
+                        <ul className='flex flex-col lg:flex-row list-none lg:ml-auto'>
+                            <li className='nav-item'>
+                                <a
+                                    className='px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75'
+                                    href='#home'
+                                >
+                                    <span className='ml-2'>Home</span>
+                                </a>
+                            </li>
+                            <li className='nav-item'>
+                                <a
+                                    className='px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75'
+                                    href='#about'
+                                >
+                                    <span className='ml-2'>About</span>
+                                </a>
+                            </li>
+                            <li className='nav-item'>
+                                <a
+                                    className='px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75'
+                                    href='/projects'
+                                >
+                                    <span className='ml-2'>Projects</span>
+                                </a>
+                            </li>
+                            <li className='nav-item'>
+                                <a
+                                    className='px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75'
+                                    href='/blogs'
+                                >
+                                    <span className='ml-2'>Blog</span>
+                                </a>
+                            </li>
+                            <li className='nav-item'>
+                                <a
+                                    className='px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75'
+                                    href='#contact'
+                                >
+                                    <span className='ml-2'>Contact</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </header>
     );
 }
